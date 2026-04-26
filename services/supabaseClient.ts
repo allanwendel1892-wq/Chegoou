7cbbbae7a9973609dc4d7677ff4ea56edb4a3880
allanwendel1892-wq <allanwendel1892@gmail.com>
2026-04-25 23:42:26 -0300

    Update supabaseClient.ts

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -1,14 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// ------------------------------------------------------------------
-// NOVA CONFIGURAÇÃO (APONTANDO PARA O EASYPANEL)
-// ------------------------------------------------------------------
-
-const SUPABASE_URL = 'https://chegoou-api.allanwendel-wq.easypanel.host'; 
-
-export const SUPABASE_ANON_KEY = 'chave-temporaria-easypanel';
-
-console.log("🟢 Conectado ao Servidor Próprio Easypanel");
+const SUPABASE_URL = 'https://modo-fila-supabase.znzrqn.easypanel.host'; 
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
