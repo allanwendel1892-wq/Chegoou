c10563c8ef2c0175f78e471bbeb0197dfc220778
allanwendel1892-wq <allanwendel1892@gmail.com>
2026-07-15 04:35:19 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#EA1D2C',
+        brandHover: '#B9121F',
+        brandLight: '#FEF2F2'
+      },
+      spacing: {
+        'safe-top': 'env(safe-area-inset-top)',
+        'safe-bottom': 'env(safe-area-inset-bottom)',
+        'safe-left': 'env(safe-area-inset-left)',
+        'safe-right': 'env(safe-area-inset-right)',
+      }
+    }
+  },
+  plugins: [],
+}
